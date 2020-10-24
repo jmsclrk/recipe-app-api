86eeff69abd068f8772f5bcb114fd7146e416a57
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2020-10-24 18:19:14 +0100

    Add ValueError for invalid user email

--- app/core/models.py
+++ app/core/models.py
@@ -2,23 +2,27 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                        PermissionsMixin
 
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
+        if not email:
+            raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using=self._db) #good practice to support multiple databases
+        user.save(using=self._db)  # good practice, supports multiple databases
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False) #special function used to create staff user
+    is_staff = models.BooleanField(default=False)  # create with special func
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email' #allows us to use email field instead of username
+    USERNAME_FIELD = 'email'  # allows email field used as username
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -19,3 +20,8 @@ class ModelTests(TestCase):
         email = 'test@JAMESCLARK.COM'
         user = get_user_model().objects.create_user(email, 'Testpass123')
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email address raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'Testpass123')
