5bd648e60a716e6c1f8d48ba1acc80b55f9d0f7e
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2020-10-24 18:31:05 +0100

    Add create superuser function

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -25,3 +25,13 @@ class ModelTests(TestCase):
         """Test creating user with no email address raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'Testpass123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@jamesclark.com',
+            'Testpass123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
